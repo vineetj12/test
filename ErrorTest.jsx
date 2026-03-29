8f7b52882a8f1874257b0b9555aaa5a1501e57fc
vineet <155095640+0123vineet@users.noreply.github.com>
2026-03-30 00:08:20 +0530

    add: build time errors for testing

--- ErrorTest.jsx
+++ ErrorTest.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { nonExistentFunction } from './nonexistent-module.js';
 
 export default function ErrorTest() {
-  // This will throw an error when rendered
-  throw new Error('This is an intentional error for testing!');
+  // Undefined variable - will cause build error
+  const result = undefinedVariable;
   
+  // Syntax error - missing closing parenthesis
+  const obj = { name: 'test', value: 123;
+  
+  // JSX error - missing closing tag
   return (
     <div>
-      <h1>This component has an intentional error</h1>
+      <h1>Build Error Component</h1>
+      <p>This has multiple build errors
+      <button onClick={nonExistentFunction}>Click me</button>
     </div>
   );
 }
